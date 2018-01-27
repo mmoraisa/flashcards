c7d6f2f20dda3adc626ab0dfb35927ef5d7d4009
Murillo de Morais <mm@in3d.com.br>
2018-01-27 20:09:39 -0200

    fix: removed default INITIAL STATE

--- application/helpers/AsyncStorageControl.js
+++ application/helpers/AsyncStorageControl.js
@@ -1,30 +1,7 @@
 import { AsyncStorage } from 'react-native'
 import { FLASHCARDS_STORAGE_KEY } from './Config'
 
-const INITIAL_DECKS = {
-    React: {
-        title: 'React',
-        questions: [
-            {
-                question: 'What is React?',
-                answer: 'A library for managing user interfaces'
-            },
-            {
-                question: 'Where do you make Ajax requests in React?',
-                answer: 'The componentDidMount lifecycle event'
-            }
-        ]
-    },
-    JavaScript: {
-        title: 'JavaScript',
-        questions: [
-            {
-                question: 'What is a closure?',
-                answer: 'The combination of a function and the lexical environment within which that function was declared.'
-            }
-        ]
-    }
-}
+const INITIAL_DECKS = {}
 
 function setInitialDecks() {
     return AsyncStorage.getItem(FLASHCARDS_STORAGE_KEY)
